6f6689d47c639cc97fb455b990e71218db96de35
hakumai35 <36781560+hakumai35@users.noreply.github.com>
2025-03-29 01:58:38 +0900

    main.py

--- main.py
+++ main.py
@@ -8,6 +8,44 @@ from personality import get_response
 app = Flask(__name__)
 
 # 環境変数から取得
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    signature = request.headers.get("X-Line-Signature")
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return "OK"
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    user_message = event.message.text
+    reply = get_response(user_message)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply)
+    )
+
+if __name__ == "__main__":
+    app.run()from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import os
+from personality import get_response
+
+app = Flask(__name__)
+
+# 環境変数から取得
 LINE_CHANNEL_ACCESS_TOKEN = os.getenv("7PRWsisvLAGTQ79CYmH6HatCtJz4rRK75gRPe7rQ9CVYFdED9iAC2OE0jiYxjHlG+Jn446WoANj7coi28eVCcv6P2Ad2/Ky70DUXZt7k/tQHExsCQLHRGI1XnqP6HY07JV8LtNT0AUzv2gKng0EwdQdB04t89/1O/w1cDnyilFU=")
 LINE_CHANNEL_SECRET = os.getenv("6661a9bc0547457b87553095fe6534f6")
 
