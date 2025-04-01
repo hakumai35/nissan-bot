f9fa5533516f7eea496a9d4b43686a8457f2cb2f
hakumai35 <36781560+hakumai35@users.noreply.github.com>
2025-04-02 04:52:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,26 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from responder import generate_niisan_reply
 import os
+
+from responder import generate_niisan_reply
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ["LINE_CHANNEL_ACCESS_TOKEN"])
-handler = WebhookHandler(os.environ["LINE_CHANNEL_SECRET"])
+# 環境変数からトークンを取得
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
 
-@app.route("/")
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+@app.route('/')
 def home():
-    return "LINE Bot is running!"
+    return 'Bot is running'
 
-@app.route("/webhook", methods=["POST"])
+@app.route('/webhook', methods=['POST'])
 def webhook():
-    signature = request.headers["X-Line-Signature"]
+    signature = request.headers.get('X-Line-Signature', '')
     body = request.get_data(as_text=True)
 
     try:
@@ -24,14 +29,19 @@ def webhook():
     except InvalidSignatureError:
         abort(400)
 
-    return "OK"
+    return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     user_id = event.source.user_id
     user_message = event.message.text
+
     reply = generate_niisan_reply(user_id, user_message)
+
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=reply)
     )
+
+if __name__ == '__main__':
+    app.run()
