2bbdb70405d33cc8603ae00979eb08114871e481
hakumai35 <36781560+hakumai35@users.noreply.github.com>
2025-04-02 03:21:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,27 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from responder import generate_niisan_reply
 import os
+from responder import generate_niisan_reply
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ["LINE_CHANNEL_ACCESS_TOKEN"])
-handler = WebhookHandler(os.environ["LINE_CHANNEL_SECRET"])
+# 環境変数から読み込む
+LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.environ.get("LINE_CHANNEL_SECRET")
 
-@app.route("/")
-def hello():
-    return "Hello from Niisan bot!"
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-@app.route("/webhook", methods=["POST"])
+# 動作確認用
+@app.route('/')
+def index():
+    return "Bot is alive"
+
+# Webhookエンドポイント
+@app.route('/webhook', methods=['POST'])
 def webhook():
-    signature = request.headers["X-Line-Signature"]
+    signature = request.headers.get("X-Line-Signature")
     body = request.get_data(as_text=True)
 
     try:
@@ -26,6 +32,7 @@ def webhook():
 
     return "OK"
 
+# メッセージ受信時の処理
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     user_id = event.source.user_id
