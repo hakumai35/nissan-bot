30d7bcd824d1718be0f33993a255048cc9ac9923
hakumai35 <36781560+hakumai35@users.noreply.github.com>
2025-04-02 00:12:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
+# main.py
+
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from personality import generate_reply
 import os
+from responder import generate_niisan_reply
 
 app = Flask(__name__)
-
-line_bot_api = LineBotApi(os.environ["LINE_CHANNEL_ACCESS_TOKEN"])
-handler = WebhookHandler(os.environ["LINE_CHANNEL_SECRET"])
-
-@app.route("/")
-def index():
-    return "Bot is alive"
+line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    signature = request.headers["X-Line-Signature"]
+    signature = request.headers.get("X-Line-Signature", "")
     body = request.get_data(as_text=True)
+
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
+
     return "OK"
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     user_message = event.message.text
-    reply = generate_reply(user_message)
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
+    user_id = event.source.user_id
+    reply = generate_niisan_reply(user_id, user_message)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply)
+    )
 
 if __name__ == "__main__":
     app.run()
