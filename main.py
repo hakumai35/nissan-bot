bdbda636cbfd81c3c02c13d00dc9de9bc75b496e
hakumai35 <36781560+hakumai35@users.noreply.github.com>
2025-04-02 03:18:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,21 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import os
 from responder import generate_niisan_reply
+import os
 
 app = Flask(__name__)
 
-# 環境変数から取得
-LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN")
-LINE_CHANNEL_SECRET = os.environ.get("LINE_CHANNEL_SECRET")
+line_bot_api = LineBotApi(os.environ["LINE_CHANNEL_ACCESS_TOKEN"])
+handler = WebhookHandler(os.environ["LINE_CHANNEL_SECRET"])
 
-if not LINE_CHANNEL_ACCESS_TOKEN or not LINE_CHANNEL_SECRET:
-    raise ValueError("環境変数にLINEのトークンが設定されていません。")
-
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(LINE_CHANNEL_SECRET)
-
-@app.route("/", methods=["GET"])
-def index():
-    return "にゃ〜ん、ニーサンは動いてるよ。"
+@app.route("/")
+def hello():
+    return "Hello from Niisan bot!"
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    signature = request.headers.get("X-Line-Signature", "")
+    signature = request.headers["X-Line-Signature"]
     body = request.get_data(as_text=True)
 
     try:
@@ -35,14 +28,10 @@ def webhook():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    user_message = event.message.text
     user_id = event.source.user_id
+    user_message = event.message.text
     reply = generate_niisan_reply(user_id, user_message)
-
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=reply)
-    )
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
 
 if __name__ == "__main__":
     app.run()
