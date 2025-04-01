23a603a440429951573557a70e2e004010f2d21e
hakumai35 <36781560+hakumai35@users.noreply.github.com>
2025-04-02 04:53:42 +0900

    Update memory.py

--- memory.py
+++ memory.py
@@ -2,16 +2,16 @@ import redis
 import json
 import os
 
-r = redis.from_url(os.environ["REDIS_URL"], decode_responses=True)
+REDIS_URL = os.environ.get("REDIS_URL")
+r = redis.Redis.from_url(REDIS_URL)
 
 def get_history(user_id):
     key = f"history:{user_id}"
     history_json = r.get(key)
     if history_json:
         return json.loads(history_json)
-    else:
-        return []
+    return []
 
 def save_history(user_id, history):
     key = f"history:{user_id}"
-    r.set(key, json.dumps(history))
+    r.set(key, json.dumps(history), ex=60*60*12)  # 12時間有効
