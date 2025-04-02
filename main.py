61040d381bcbf68300e57a06c64b8fccdbd65fd5
hakumai35 <36781560+hakumai35@users.noreply.github.com>
2025-04-03 01:31:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,18 @@
-from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from flask import Flask, request
 from personality import generate_reply
-import os
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ["LINE_CHANNEL_ACCESS_TOKEN"])
-handler = WebhookHandler(os.environ["LINE_CHANNEL_SECRET"])
+@app.route('/')
+def index():
+    return "Bot is running!"
 
-@app.route("/webhook", methods=["POST"])
+@app.route('/webhook', methods=['POST'])
 def webhook():
-    signature = request.headers["X-Line-Signature"]
-    body = request.get_data(as_text=True)
-
+    data = request.json
     try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return "OK"
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    user_input = event.message.text
-    reply = generate_reply(user_input)
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
-
-if __name__ == "__main__":
-    app.run()
+        user_input = data['events'][0]['message']['text']
+        reply = generate_reply(user_input)
+        return reply, 200
+    except Exception as e:
+        return str(e), 500
